8e16132695e2780339a231924cb3fe771121e093
Ramyani <ramyani@Surendras-Mac-mini.local>
2012-08-10 13:25:08 +0530

    add a module Micropost, create association between User and Micropost and show microposts in the user's show page

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  has_many :microposts, dependent: :destroy
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
-before_save :create_remember_token
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,7 +15,7 @@ before_save :create_remember_token
   validates :password_confirmation, presence: true
 
 
-def create_remember_token
-  self.remember_token = SecureRandom.urlsafe_base64
-end
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
